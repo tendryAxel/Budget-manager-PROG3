87a92f21301a01fba8349a440938456ac92aee05
tendryAxel <hei.tendry@gmail.com>
2023-12-02 07:19:53 +0300

    feat : initial commit

---
+++ src/test/java/school/hei/budgetmanager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.budgetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
