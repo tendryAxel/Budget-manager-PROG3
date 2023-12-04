5ae4c3666ab09f52a94c21c4b124621a38a97e09
Maharavo1 <hei.maharavo@gmail.com>
2023-12-04 23:41:23 +0300

    feat: Implement CurrencyCrudOperations class for CRUD operations on CurrencyModel

--- src/src/operation/CurrencyCrudOperations.java
+++ src/src/operation/CurrencyCrudOperations.java
@@ -2,32 +2,58 @@ package operation;
 
 import model.CurrencyModel;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CurrencyCrudOperations implements CrudOperations<CurrencyModel> {
+public class CurrencyCrudOperations implements CrudOperations<CurrencyModel>{
     @Override
     public List<CurrencyModel> findAll() throws SQLException {
-        ResultSet resultSet = connectionDB.getConnection().prepareStatement("SELECT * FROM \"currency\"").executeQuery();
-        List<CurrencyModel> result = new ArrayList<>();
+        String sql = "SELECT * FROM currency";
+        List<CurrencyModel> AllCurrency = new ArrayList<>();
+
+        ResultSet resultSet = connectionDB.getConnection().prepareStatement(sql).executeQuery();
         while (resultSet.next()){
-            result.add(new CurrencyModel(
+            AllCurrency.add(new CurrencyModel(
                     resultSet.getInt("id"),
                     resultSet.getString("name")
             ));
         }
-        return result;
+        return AllCurrency;
+    }
+
+    public List<CurrencyModel> saveAll(List<CurrencyModel> toSave)  {
+        String sql = "INSERT INTO currency (id , name) VALUES (?, ?)";
+        List<CurrencyModel> SaveCurrency = new ArrayList<>();
+        try(PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)){
+            for (CurrencyModel currencyModel : toSave){
+                preparedStatement.setInt(1, currencyModel.getId());
+                preparedStatement.setString(2, currencyModel.getName());
+                int rowsAffected = preparedStatement.executeUpdate();
+                if (rowsAffected > 0) {
+                    SaveCurrency.add(currencyModel);
+                }
+            }
+        }
+        catch (SQLException e){
+            throw new RuntimeException();
+        }
+        return SaveCurrency;
     }
 
     @Override
-    public CurrencyModel save(CurrencyModel toSave) {
-        return null;
-    }
-
-    @Override
-    public List saveAll(List toSave) {
-        return null;
+    public CurrencyModel save(CurrencyModel toSave)  {
+        String sql = "INSERT INTO currency (id , name) VALUES (?, ?)";
+        try(PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)){
+            preparedStatement.setInt(1, toSave.getId());
+            preparedStatement.setString(2,toSave.getName());
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new RuntimeException();
+        }
+        return toSave;
     }
 }
