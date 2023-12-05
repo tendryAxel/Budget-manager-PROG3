449b70fcf05395400382b506eab9b8b797209de9
Maharavo1 <hei.maharavo@gmail.com>
2023-12-05 07:40:04 +0300

    feat: Update CurrencyModel class

--- src/src/model/CurrencyModel.java
+++ src/src/model/CurrencyModel.java
@@ -4,6 +4,11 @@ public class CurrencyModel {
   private   int id;
    private String name;
 
+    public CurrencyModel(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
