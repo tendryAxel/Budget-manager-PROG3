5577ba57304ed2adaf7ea463642d835e1f73cf2e
tendryAxel <hei.tendry@gmail.com>
2023-12-06 14:45:27 +0300

    fix : remove unused importation

--- src/src/repository/TransactionCrudOperation.java
+++ src/src/repository/TransactionCrudOperation.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
