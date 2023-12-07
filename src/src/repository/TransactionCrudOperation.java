dfc77ba5b00010f95a10420308188f7d1f91d415
tendryAxel <hei.tendry@gmail.com>
2023-12-07 22:06:35 +0300

    feat : create function to get transactions of one account between two timestamps

--- src/src/repository/TransactionCrudOperation.java
+++ src/src/repository/TransactionCrudOperation.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,5 +61,33 @@ import java.util.List;
                 throw new RuntimeException();
             }
             return toSave;
+        }
+
+
+        public List<TransactionModel> findAllByIdAccountAndDate(int id, LocalDateTime startDate, LocalDateTime endDate) throws SQLException {
+            String sql = "SELECT * FROM \"transaction\" " +
+                    "INNER JOIN \"account\" " +
+                    "ON \"transaction\".id_account = \"account\".id " +
+                    "WHERE \"account\".id = ?" +
+                    "AND \"transaction\".transaction_date BETWEEN ? AND ? " +
+                    "ORDER BY \"transaction\".transaction_date DESC ";
+            PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(startDate));
+            preparedStatement.setTimestamp(3, Timestamp.valueOf(endDate));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<TransactionModel> transactions = new ArrayList<>();
+
+            while (resultSet.next()){
+                transactions.add(new TransactionModel(
+                        resultSet.getInt("\"transaction\".id"),
+                        resultSet.getInt("value"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("id_account"),
+                        resultSet.getTimestamp("transaction_date").toLocalDateTime()
+                ));
+            }
+
+            return transactions;
         }
 }
