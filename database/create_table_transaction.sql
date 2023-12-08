7d1dbf7c922be31ecf71daeb50c2847218e10387
tendryAxel <hei.tendry@gmail.com>
2023-12-08 15:05:12 +0300

    fix : database error

--- database/create_table_transaction.sql
+++ database/create_table_transaction.sql
@@ -3,7 +3,6 @@ CREATE type TransactionType AS ENUM ('DEBIT', 'CREDIT');
 -- Creation de la table
 CREATE TABLE IF NOT EXISTS "transaction" (
     id SERIAL PRIMARY KEY,
-    value INT NOT NULL,
     label VARCHAR(100) NOT NULL,
     amount double precision NOT NULL,
     transaction_date timestamp NOT NULL DEFAULT current_timestamp,
@@ -12,13 +11,13 @@ CREATE TABLE IF NOT EXISTS "transaction" (
 );
 
 -- Insertion 1
-INSERT INTO "transaction" (value, label, amount, type, id_account, transaction_date)
-VALUES (100, 'Deposit', 100.0, 'CREDIT', 1, '2023-12-02 10:00:00');
+INSERT INTO "transaction" (label, amount, type, id_account, transaction_date)
+VALUES ('Deposit', 100.0, 'CREDIT', 1, '2023-12-02 10:00:00');
 
 -- Insertion 2
-INSERT INTO "transaction" (value, label, amount, type, id_account, transaction_date)
-VALUES (-50, 'Withdrawal', 50.0, 'DEBIT', 2, '2023-12-02 12:30:00');
+INSERT INTO "transaction" (label, amount, type, id_account, transaction_date)
+VALUES ('Withdrawal', 50.0, 'DEBIT', 2, '2023-12-02 12:30:00');
 
 -- Insertion 3
-INSERT INTO "transaction" (value, label, amount, type, id_account, transaction_date)
-VALUES (200, 'Deposit', 200.0, 'CREDIT', 3, '2023-12-02 15:45:00');
+INSERT INTO "transaction" (label, amount, type, id_account, transaction_date)
+VALUES ('Deposit', 200.0, 'CREDIT', 3, '2023-12-02 15:45:00');
