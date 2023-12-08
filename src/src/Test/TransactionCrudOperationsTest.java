920eb7c78282eb5a3cffa89872b317c7444503c2
Maharavo1 <hei.maharavo@gmail.com>
2023-12-08 12:39:54 +0300

    feat: Update TransactionCrudTest

--- src/src/Test/TransactionCrudOperationsTest.java
+++ src/src/Test/TransactionCrudOperationsTest.java
@@ -1,10 +1,11 @@
 package Test;
 
 import model.TransactionModel;
+import model.TransactionType;
 import repository.TransactionCrudOperation;
 
 import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,21 +19,23 @@ public class TransactionCrudOperationsTest {
     }
 
     public static void transactionTest() throws SQLException {
-        TransactionCrudOperation transactionCrudOperations = new TransactionCrudOperation();
+        TransactionCrudOperation transactionCrudOperation = new TransactionCrudOperation();
 
-        List<TransactionModel> result = transactionCrudOperations.findAll();
+        List<TransactionModel> result = transactionCrudOperation.findAll();
         System.out.println("Toutes les Transactions :");
         for (TransactionModel t : result) {
             System.out.println(t);
         }
 
-        TransactionModel transactionModel = new TransactionModel(1, 500, "Purchase", 2, Timestamp.valueOf("2023-12-04").toLocalDateTime());
-        transactionCrudOperations.save(transactionModel);
+
+        LocalDateTime transaction_date = LocalDateTime.now();
+        TransactionModel transactionModel = new TransactionModel(1, "Cadeau Noel", 23000.0, transaction_date , TransactionType.DEBIT , 1);
+        transactionCrudOperation.save(transactionModel);
 
         List<TransactionModel> transactionsToSave = new ArrayList<>();
-        transactionsToSave.add(new TransactionModel(2, 1000, "Salary", 3, Timestamp.valueOf("2023-12-05").toLocalDateTime()));
+        transactionsToSave.add(new TransactionModel(2, "Salaire", 23000.0 , transaction_date ,TransactionType.CREDIT , 2));
 
-        List<TransactionModel> savedTransactions = transactionCrudOperations.saveAll(transactionsToSave);
+        List<TransactionModel> savedTransactions = transactionCrudOperation.saveAll(transactionsToSave);
         System.out.println("Transactions enregistrées :");
         for (TransactionModel tr : savedTransactions) {
             System.out.println(tr);
