e096c09b8385b31f94996c7ba1201af7d346efb7
Maharavo1 <hei.maharavo@gmail.com>
2023-12-08 16:58:34 +0300

    feat: Add table transfet

---
+++ database/create_table_transfert.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS "transfert" (
+    id INT NOT NUll,
+    id_debtor INT NOT NUll,
+    id_credit INT NOT NUll,
+    transfer_date TIMESTAMP NOT NULL
+);
+
+-- Insérer des données dans la table "transfert"
+INSERT INTO transfert (id, id_debtor, id_credit, transfer_date) VALUES
+(1, 1001, 2001, '2023-01-01 12:00:00'),
+(2, 1002, 2002, '2023-02-15 08:30:00'),
+(3, 1003, 2003, '2023-03-20 16:45:00');
