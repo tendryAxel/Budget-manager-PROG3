70c02a2d039e9fb68163ce1a8c8224ea507c3745
Maharavo1 <hei.maharavo@gmail.com>
2023-12-08 12:31:38 +0300

    feat: Update AccountCrudOperations

--- src/src/repository/AccountCrudOperations.java
+++ src/src/repository/AccountCrudOperations.java
@@ -1,61 +1,73 @@
 package repository;
 
 import model.AccountModel;
+import model.AccountType;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class AccountCrudOperations implements CrudOperations <AccountModel>{
-        @Override
-        public List<AccountModel> findAll() throws SQLException {
-            String sql = "SELECT * FROM \"account\" ";
-            List<AccountModel> AllAccount = new ArrayList<>();
-            ResultSet resultSet = connectionDB.getConnection().prepareStatement(sql).executeQuery();
-            while (resultSet.next()){
-                AllAccount.add(new AccountModel(
-                        resultSet.getInt("id"),
-                        resultSet.getString("name"),
-                        resultSet.getInt("id_currency")
-                ));
-            }
-            return AllAccount;
+public class AccountCrudOperations implements CrudOperations <AccountModel>{
+    @Override
+    public List<AccountModel> findAll() throws SQLException {
+        String sql = "SELECT * FROM \"account\" ";
+        List<AccountModel> AllAccount = new ArrayList<>();
+        ResultSet resultSet = connectionDB.getConnection().prepareStatement(sql).executeQuery();
+        while (resultSet.next()){
+            AllAccount.add(new AccountModel(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getBigDecimal("balance"),
+                    resultSet.getTimestamp("updateDate "),
+                    resultSet.getInt("id_currency"),
+                    AccountType.valueOf(resultSet.getString("type"))
+            ));
         }
+        return AllAccount;
+    }
 
-        @Override
-        public List<AccountModel> saveAll(List<AccountModel> toSave) {
-            String sql = "INSERT INTO \"account\" (name , id_currency) VALUES (?,?)";
-            List<AccountModel> SaveAccount = new ArrayList<>();
-            try(PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)){
-                for (AccountModel accountModel : toSave){
-                    preparedStatement.setString(1, accountModel.getName());
-                    preparedStatement.setInt(2, accountModel.getId_currency());
+    @Override
+    public List<AccountModel> saveAll(List<AccountModel> toSave) {
+        String sql = "INSERT INTO \"account\" (name ,balance , updadeDate , id_currency , type) VALUES (?,?,?,?)";
+        List<AccountModel> SaveAccount = new ArrayList<>();
+        try(PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)){
+            for (AccountModel accountModel : toSave){
+                preparedStatement.setString(1, accountModel.getName());
+                preparedStatement.setBigDecimal(2,accountModel.getBalance());
+                preparedStatement.setTimestamp(3,accountModel.getUpdateDate());
+                preparedStatement.setInt(4,accountModel.getId_currency());
+                preparedStatement.setObject(5 , accountModel.getType() , Types.OTHER);
 
-                    int rowAffected = preparedStatement.executeUpdate();
-                    if (rowAffected > 0){
-                        SaveAccount.add(accountModel);
-                    }
+                int rowAffected = preparedStatement.executeUpdate();
+                if (rowAffected > 0){
+                    SaveAccount.add(accountModel);
                 }
             }
-            catch (SQLException e){
-                throw new RuntimeException();
-            }
-            return SaveAccount;
         }
-
-        @Override
-        public AccountModel save(AccountModel toSave)  {
-            String sql = "INSERT INTO \"account\" (name, id_currency) VALUES (?,?) ";
-            try (PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)) {
-                preparedStatement.setString(1, toSave.getName());
-                preparedStatement.setInt(2, toSave.getId_currency());
-                preparedStatement.executeUpdate();
-            } catch (SQLException e) {
-                throw new RuntimeException();
-            }
-            return null;
+        catch (SQLException e){
+            throw new RuntimeException();
         }
-
+        return SaveAccount;
     }
+
+    @Override
+    public AccountModel save(AccountModel toSave)  {
+        String sql = "INSERT INTO \"account\" (name ,balance , updadeDate , id_currency , type) VALUES (?,?,?,?) ";
+        try (PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, toSave.getName());
+            preparedStatement.setBigDecimal(2,toSave.getBalance());
+            preparedStatement.setTimestamp(3,toSave.getUpdateDate());
+            preparedStatement.setInt(4, toSave.getId_currency());
+            preparedStatement.setObject(5 , toSave.getType() ,Types.OTHER);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+        return null;
+    }
+
+}
