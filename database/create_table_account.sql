df8c3b4807dfa953d657079abc4929f729683921
Maharavo1 <hei.maharavo@gmail.com>
2023-12-08 12:23:02 +0300

    feat: Update table account

--- database/create_table_account.sql
+++ database/create_table_account.sql
@@ -1,14 +1,24 @@
+CREATE type AccountType AS ENUM ('Banque', 'Espèce' , 'MobileMoney');
+
 CREATE TABLE IF NOT EXISTS "account" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
-    id_currency INT REFERENCES "currency"(id)
+    balance DECIMAL (10 , 2) NOT NULL,
+    updatedDate TIMESTAMP NOT NULL,
+    id_currency INT REFERENCES "currency"(id),
+    type AccountType NOT NULL
 );
 
 -- Insertion 1
-INSERT INTO "account" (name, id_currency) VALUES ('Savings Account', 1);
+INSERT INTO "account" (name, balance, updatedDate, id_currency, type)
+VALUES ('Savings Account', 45000.0, CURRENT_TIMESTAMP, 1, 'Banque');
 
 -- Insertion 2
-INSERT INTO "account" (name, id_currency) VALUES ('Checking Account', 2);
+INSERT INTO "account" (name, balance, updatedDate, id_currency, type)
+VALUES ('Checking Account', 23000.0, CURRENT_TIMESTAMP, 2, 'Espèce');
 
 -- Insertion 3
-INSERT INTO "account" (name, id_currency) VALUES ('Investment Account', 3);
+INSERT INTO "account" (name, balance, updatedDate, id_currency, type)
+VALUES ('Investment Account', 12000.0, CURRENT_TIMESTAMP, 3, 'MobileMoney');
+
+
