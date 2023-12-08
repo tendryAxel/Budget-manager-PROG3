88df5dc30dffb23e53cb57f5a2d843454356d5bc
Maharavo1 <hei.maharavo@gmail.com>
2023-12-08 12:25:46 +0300

    feat: Update AcountModel class

--- src/src/model/AccountModel.java
+++ src/src/model/AccountModel.java
@@ -1,14 +1,24 @@
 package model;
 
-public class AccountModel {
-  private   int id;
-   private String name;
- private    int id_currency;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
-    public AccountModel(int id, String name, int id_currency) {
+public class AccountModel {
+    private   int id;
+    private String name;
+    private BigDecimal balance;
+    private Timestamp updateDate;
+    private    int id_currency;
+    private AccountType type;
+     private List<Transaction> transactions ;
+
+    public AccountModel(int id, String name, BigDecimal balance, Timestamp updateDate, int id_currency, AccountType type) {
         this.id = id;
         this.name = name;
+        this.balance = balance;
+        this.updateDate = updateDate;
         this.id_currency = id_currency;
+        this.type = type;
     }
 
     public int getId() {
@@ -27,6 +37,22 @@ public class AccountModel {
         this.name = name;
     }
 
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+    }
+
     public int getId_currency() {
         return id_currency;
     }
@@ -35,12 +61,23 @@ public class AccountModel {
         this.id_currency = id_currency;
     }
 
+    public AccountType getType() {
+        return type;
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
+
     @Override
     public String toString() {
         return "AccountModel{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", updateDate=" + updateDate +
                 ", id_currency=" + id_currency +
+                ", type=" + type +
                 '}';
     }
 }
