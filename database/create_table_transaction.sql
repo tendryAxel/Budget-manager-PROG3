b6efaff00ec9f717236194c84626a09b80498005
Maharavo1 <hei.maharavo@gmail.com>
2023-12-14 14:25:04 +0300

    fix : alter table transaction

--- database/create_table_transaction.sql
+++ database/create_table_transaction.sql
@@ -8,17 +8,18 @@ CREATE TABLE IF NOT EXISTS "transaction" (
     transaction_date timestamp NOT NULL DEFAULT current_timestamp,
     type TransactionType NOT NULL,
     id_account INT REFERENCES "account"(id_account),
-    id_subcategory INT REFERENCES "subcategory"(id_subcategory)
+    id_subcategory INT REFERENCES "subcategory"(id_subcategory),
+    id_currency INT REFERENCES "currency" (id_currency)
 );
 
 -- Insertion 1
-INSERT INTO "transaction" (label, amount, type, id_account, transaction_date)
-VALUES ('Deposit', 100.0, 'CREDIT', 1, '2023-12-02 10:00:00');
+INSERT INTO "transaction" (label, amount, type, id_account, transaction_date , id_subcategory , id_currency)
+VALUES ('Deposit', 100.0, 'CREDIT', 1, '2023-12-02 10:00:00' ,56, 2);
 
 -- Insertion 2
-INSERT INTO "transaction" (label, amount, type, id_account, transaction_date)
-VALUES ('Withdrawal', 50.0, 'DEBIT', 2, '2023-12-02 12:30:00');
+INSERT INTO "transaction" (label, amount, type, id_account, transaction_date , id_subcategory ,id_currency)
+VALUES ('Withdrawal', 50.0, 'DEBIT', 2, '2023-12-02 12:30:00' ,57 , 2);
 
 -- Insertion 3
-INSERT INTO "transaction" (label, amount, type, id_account, transaction_date)
-VALUES ('Deposit', 200.0, 'CREDIT', 3, '2023-12-02 15:45:00');
+INSERT INTO "transaction" (label, amount, type, id_account, transaction_date , id_subcategory , id_currency)
+VALUES ('Deposit', 200.0, 'CREDIT', 3, '2023-12-02 15:45:00' , 58,2);
