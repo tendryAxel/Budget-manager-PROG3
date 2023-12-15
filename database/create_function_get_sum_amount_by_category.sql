24695814fd7005e83aa6ac9a5260c04917f1bfc1
Maharavo1 <hei.maharavo@gmail.com>
2023-12-15 07:32:05 +0300

    fix : Rename and update table create_function_get_sum_amount_by_category.sql

--- database/create_function_get_sum_amount_by_category.sql
+++ database/create_function_get_sum_amount_by_category.sql
@@ -1,28 +1,28 @@
-CREATE OR REPLACE FUNCTION get_sum_amount_by_category(
-    account_id INT,
-    start_date TIMESTAMP,
+ CREATE OR REPLACE FUNCTION get_sum_amount_by_category(
+   account_id INT,
+   start_date TIMESTAMP,
     end_date TIMESTAMP
 ) RETURNS TABLE (
-    category_name VARCHAR(100),
-    total_amount DOUBLE PRECISION
+   restaurant DOUBLE PRECISION,
+   salary DOUBLE PRECISION
 )
-AS
-$$
+ AS
+ $$
 BEGIN
-    RETURN QUERY
-    SELECT
-        c.name AS category_name,
-        COALESCE(SUM(t.amount), 0) AS total_amount
+  RETURN QUERY
+   SELECT
+        COALESCE(SUM(CASE WHEN c.name = 'Restaurant' THEN t.amount ELSE 0 END), 0)  -
+       COALESCE(SUM(CASE WHEN c.name = 'Salaire' THEN t.amount ELSE 0 END), 0)
     FROM
-        "category" c
+        "account" a
+   LEFT JOIN
+        "transaction" t ON t.id_account = a.id_account
     LEFT JOIN
-        "subcategory" s ON c.id_category = s.id_category
+        "subcategory" s ON s.id_subcategory = t.id_subcategory
     LEFT JOIN
-        "transaction" t ON s.id_subcategory = t.id_subcategory
+       "category" c ON s.id_category = c.id_category
     WHERE
-        t.id_account = account_id
-        AND t.transaction_date BETWEEN start_date AND end_date
-    GROUP BY
-        c.name;
-END;
-$$ LANGUAGE plpgsql;
+        a.id_account = account_id AND
+        t.transaction_date BETWEEN start_date AND end_date;
+   END;
+ $$ LANGUAGE plpgsql;
