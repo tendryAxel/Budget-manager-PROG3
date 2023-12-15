81da5ef0eea31a8b55bf6b1188940ad81df84304
Maharavo1 <hei.maharavo@gmail.com>
2023-12-15 14:54:45 +0300

    fix : Update Main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,4 +1,5 @@
 import Test.AccountCrudOperationsTest;
+import Test.BalanceCrudOperationsTest;
 import Test.CurrencyCrudOperationsTest;
 import Test.TransactionCrudOperationsTest;
 
@@ -8,6 +9,9 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         AccountCrudOperationsTest.accountTest();
         CurrencyCrudOperationsTest.currencyTest();
+        TransactionCrudOperationsTest.transactionTest();
+        CurrencyCrudOperationsTest.currencyTest();
+        BalanceCrudOperationsTest.balanceTest();
         TransactionCrudOperationsTest.transactionTest();
     }
 }
