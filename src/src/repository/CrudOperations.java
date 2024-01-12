c58de38a06f08fedf57d4a463ccb1a9a7fd0620f
tendryAxel <hei.tendry@gmail.com>
2024-01-12 09:11:15 +0300

    feat : generic type ID

--- src/src/repository/CrudOperations.java
+++ src/src/repository/CrudOperations.java
@@ -5,10 +5,10 @@ import model.DefaultModel;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface CrudOperations<T extends DefaultModel> {
+public interface CrudOperations<T extends DefaultModel, ID> {
     List<T> findAll() throws SQLException;
     List<T> saveAll(List<T> toSave) throws SQLException;
     T save(T toSave) throws SQLException;
-    T findById(int id) throws SQLException;
-    T delete(int id) throws SQLException;
+    T findById(ID id) throws SQLException;
+    T delete(ID id) throws SQLException;
 }
