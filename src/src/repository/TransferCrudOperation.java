9a36ee0159f5d7e0ebf97f7127299c3e64d66a45
Maharavo1 <hei.maharavo@gmail.com>
2024-01-12 21:28:05 +0300

    feat : Refactor TransferCrudOperations to extend CrudOperationsImpl

--- src/src/repository/TransferCrudOperation.java
+++ src/src/repository/TransferCrudOperation.java
@@ -1,71 +1,35 @@
 package repository;
+import utils.PreparedStatementStep;
 import model.TransferModel;
+import java.sql.*;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class TransferCrudOperation extends CrudOperationsImpl<TransferModel> {
+public class TransferCrudOperation extends CrudOperationsImpl<TransferModel> {    @Override
+public TransferModel createT(ResultSet resultSet) throws SQLException {
+    return new TransferModel(
+            resultSet.getInt(TransferModel.ID),
+            resultSet.getInt(TransferModel.ID_DEBIT),
+            resultSet.getInt(TransferModel.ID_CREDIT),
+            resultSet.getTimestamp(TransferModel.TRANSFER_DATE).toLocalDateTime()
+    );
+}
+
     @Override
-    public List<TransferModel> findAll() throws SQLException {
-        String sql = String.format(
-                "SELECT * FROM \"%s\"",
-                TransferModel.TABLE_NAME
-        );
-        ResultSet resultSet = connectionDB.getConnection().prepareStatement(sql).executeQuery();
-        List<TransferModel> AllTransaction = new ArrayList<>();
-
-        while (resultSet.next()){
-            AllTransaction.add(new TransferModel(
-                    resultSet.getInt(TransferModel.ID),
-                    resultSet.getInt(TransferModel.ID_DEBIT),
-                    resultSet.getInt(TransferModel.ID_CREDIT),
-                    resultSet.getTimestamp(TransferModel.TRANSFER_DATE).toLocalDateTime()
-            ));
-        }
-
-        return AllTransaction;
+    public PreparedStatement createT(PreparedStatementStep pr, TransferModel model) throws SQLException {
+        PreparedStatement preparedStatement = pr.getPreparedStatement();
+        preparedStatement.setInt(1, model.getTransactionDebtor());
+        preparedStatement.setInt(2, model.getTransactionCredit());
+        preparedStatement.setTimestamp(3, Timestamp.valueOf(model.getDatetime()));
+        return preparedStatement;
     }
 
     @Override
-    public List<TransferModel> saveAll(List<TransferModel> toSave) {
-        String sql = String.format(
-                "INSERT INTO \"%s\" (%s,%s) VALUES(?,?)",
-                TransferModel.TABLE_NAME,
-                TransferModel.ID_DEBIT,
-                TransferModel.ID_CREDIT
-        );
-        List<TransferModel> SaveTransaction = new ArrayList<>();
-        try(PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)){
-            for (TransferModel transferModel : toSave){
-                preparedStatement.setInt(1,transferModel.getTransactionDebtor());
-                preparedStatement.setInt(2,transferModel.getTransactionCredit());
-            }
-        }
-        catch (SQLException e){
-            throw new RuntimeException();
-        }
-        return SaveTransaction;
+    public TransferModel findById(Integer id) {
+        return super.findById(id);
     }
 
     @Override
-    public TransferModel save(TransferModel toSave)  {
-        String sql = String.format(
-                "INSERT INTO \"%s\" (%s,%s) VALUES(?,?)",
-                TransferModel.TABLE_NAME,
-                TransferModel.ID_DEBIT,
-                TransferModel.ID_CREDIT
-        );
-        try(PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql)){
-            preparedStatement.setInt(1,toSave.getTransactionDebtor());
-            preparedStatement.setInt(2,toSave.getTransactionCredit());
-            preparedStatement.executeUpdate();
-        }
-        catch (SQLException e){
-            throw new RuntimeException();
-        }
-        return toSave;
+    public TransferModel delete(Integer id){
+        return super.delete(id);
     }
 }
