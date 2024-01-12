2ea3332e662264531df3f84d7c2e2705f123cf4d
Maharavo1 <hei.maharavo@gmail.com>
2024-01-12 21:47:03 +0300

    feat : Refactor CategoryCrudOperations to extend CrudOperationsImpl

---
+++ src/src/repository/CategoryCrudOperations.java
@@ -0,0 +1,33 @@
+package repository;
+import model.CategoryModel;
+import utils.PreparedStatementStep;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+
+public class CategoryCrudOperations extends  CrudOperationsImpl <CategoryModel>{
+
+    @Override
+    public CategoryModel createT(ResultSet resultSet) throws SQLException {
+        int id_category = resultSet.getInt("id_category");
+        String name = resultSet.getString("name");
+        return new CategoryModel(id_category, name);
+    }
+
+    @Override
+    public PreparedStatement createT(PreparedStatementStep pr,CategoryModel model) throws SQLException, InvocationTargetException, IllegalAccessException {
+        PreparedStatement preparedStatement = pr.getPreparedStatement();
+        preparedStatement.setString(1 , model.getName());
+        return preparedStatement;
+    }
+
+    @Override
+    public CategoryModel findById(Integer id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public CategoryModel delete(Integer id){
+        return super.delete(id);
+    }
+}
